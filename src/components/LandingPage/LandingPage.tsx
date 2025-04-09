ef6b17079c2ce766bfdc1019564415d2977e7b3c
efccc0010 <edgarfernandocarrionccoicca@gmail.com>
2025-04-09 01:24:25 -0500

    Update LandginPage

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavigationBar from "./NavigationBar/NavigationBar";
 import Header from "./Header/Header";
+import Logotypes from "./Logotypes/Logotypes";
 
 function LandingPage() {
   return (
     <>
       <NavigationBar></NavigationBar>
       <Header></Header>
+      <Logotypes></Logotypes>
     </>
   );
 }
